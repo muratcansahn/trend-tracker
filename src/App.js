120cb1ba8dab0cc1924f43a5ee0e79dc9735a60d
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-09-06 23:51:46 +0300

    finish app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,92 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+// import { fetchFromAPI } from "../src/fetchFromAPI";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [timePeriod, setTimePeriod] = useState("24h");
+  const [isAscending, setIsAscending] = useState(false);
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "b77330c3e0msh45c6f54ef5955d1p19f6d4jsnf3111eb84e00",
+      "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    },
+  };
+  useEffect(() => {
+    fetch(
+      `https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=${timePeriod}&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setCoins(response.data.coins))
+      .catch((err) => console.error(err));
+  }, [timePeriod]);
+
+  console.log(timePeriod);
+  const TimePeriods = [
+    "1h",
+    "3h",
+    "12h",
+    "24h",
+    "7d",
+    "30d",
+    "3m",
+    "1y",
+    "3y",
+    "5y",
+  ];
+
+  ////we can do it with price change !!!!!!
+
+  console.log(coins);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Name</th>
+          <th>Price</th>
+
+          <select
+            defaultValue="24h"
+            onChange={(e) => setTimePeriod(e.target.value)}
+          >
+            {TimePeriods.map((timePeriod) => (
+              <option key={timePeriod} value={timePeriod}>
+                {timePeriod}
+              </option>
+            ))}
+          </select>
+        </tr>
+      </thead>
+      <tbody>
+        {coins.map((coin) => {
+          // {
+          //   coin.change > 0
+          //     ? () => {
+          //         setIsAscending(true);
+          //       }
+          //     : "";
+          // }
+          return (
+            <tr key={coin.uuid}>
+              <td>{coin.rank}</td>
+              <td>{coin.name}</td>
+
+              {coin.sparkline.slice(0, 10).map((sparkline, i) => {
+                return (
+                  <td className={`sparkline`}>{sparkline.substring(0, 7)}</td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
-
 export default App;
